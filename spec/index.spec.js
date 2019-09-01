ab6c9338e0a709415640e98639dbee4cb6f90d3f
Mike Holm <mholm@homebot.ai>
2019-08-31 19:41:00 -0600

    update deps

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -1,12 +1,12 @@
-const svgTags = require('svg-tags');
-const jssvg = require('../index');
+const svgTags = require('svg-tags')
+const jssvg = require('../index')
 
 describe('jssvg', () => {
   it('has the tag functions', () => {
-    expect(jssvg.animate({foo: "bar"})).toEqual('<animate foo="bar"/>');
-  });
+    expect(jssvg.animate({ foo: 'bar' })).toEqual('<animate foo="bar" />')
+  })
 
   it('has all the functions', () => {
-    expect(Object.keys(jssvg).length).toEqual(svgTags.length);
-  });
-});
+    expect(Object.keys(jssvg).length).toEqual(svgTags.length)
+  })
+})
